47463aebf1a40c91e350bab1f68c6d6abf2e65bf
Mcleroi01 <musogelacarlo54@gmail.com>
2023-08-24 23:13:50 +0100

    project crud

--- js/dom.js
+++ js/dom.js
@@ -1,32 +1,80 @@
-//recuperation formulaire
-
-let table = []
-
+let listView = document.querySelector('table tbody');
+let form = document.getElementById('form');
 
 
-function newUser(e) {
 
-    console.log(e)
-
-    let name = document.getElementById("name")
-    let lastname = document.getElementById("last-name")
-    let mail = document.getElementById("mail")
-    let tr = document.createElement("tr")
-    table.appendChild(tr);
-    let td= document.createElement("td")
-    td.innerHTML=name.value
-    tr.appendChild(td)
-    name.value=""
-    
+// datas 
+let contacts = [];
 
 
-    let p=document.createElement("p")
-    p.innerHTML
-    alert(table)
 
+// add action ! 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let data = new FormData(e.target)
+
+
+    let user = {
+        'name': data.get('name'),
+        'username': data.get('username'),
+        'mail': data.get('mail'),
+
+    };
+    contacts.push(user)
+    // form.reset()
+    View()
+
+
+
+
+});
+
+function View() {
+    listView.innerHTML = ""
+
+    for (let i = 0; i < contacts.length; i++) {
+        let add = `<tr>
+        <td class="">${contacts[i].name}</td>
+        <td>${contacts[i].username}</td>
+        <td>${contacts[i].mail}</td>
+        <td><button type="button" onclick="delete" (${i})>Delete</button> <button type="button" onclick="Delete()" (${i})>Update</button></td>
+        
+        </tr>`
+        listView.innerHTML += add
+
+    }
+
+}
+function Delete(i) {
+    contacts.splice(i, 1);
+
+    View();
 
 
 }
+function reset() {
+
+    listView
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
